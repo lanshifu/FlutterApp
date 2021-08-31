92edc285254a88b649d0d881c4f5c96c132095b0
lanxiaobin <lanxiaobin@lizhi.com>
2021-09-01 00:48:08 +0800

    Initial commit

---
+++ app/src/main/java/com/lanshifu/flutterapp/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.lanshifu.flutterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
